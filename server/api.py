21fc5dfc8b5c76b71020927b06ce3ae900a35b94
imura_tatsuya <imura_tatsuya@cyberagent.co.jp>
2017-06-15 16:50:18 +0900

    jsonでget

--- server/api.py
+++ server/api.py
@@ -1,8 +1,44 @@
 import falcon
+import json
+import MySQLdb
+
+def getRanking():
+    connector = MySQLdb.connect(
+            user='root',
+            passwd='ca_sge_teamf',
+            host='0.0.0.0',
+            db='SGE')
+
+    cursor = connector.cursor()
+    cursor.execute('SELECT username, distance, weight, score FROM ranking ORDER BY score DESC LIMIT 100')
+    array = []
+    for (rank, row) in enumerate(cursor.fetchall()):
+        array.append({'rank': rank+1, 'username': row[0], 'distance': row[1], 'weight': row[2], 'score': row[3]})
+
+    cursor.close
+    connector.close
+    return array
+
+def getRank(score):
+    if score.isdigit():
+        connector = MySQLdb.connect(
+                user='root',
+                passwd='ca_sge_teamf',
+                host='0.0.0.0',
+                db='SGE')
+
+        cursor = connector.cursor()
+        cursor.execute('SELECT COUNT(*) FROM ranking WHERE score > ' + score)
+        rank = cursor.fetchone()[0] + 1
+        cursor.close
+        connector.close
+        return rank
+    return 0
+
 
 class RankingResource:
     def on_get(self, req, res):
-        res.body = "hoge"
+        res.body = json.dumps(getRanking())
 
     def on_post(self, req, res):
         res.body = "hogehoge"
